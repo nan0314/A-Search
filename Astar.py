de59fc753be220c80c42993278c92e14255505de
nan0314 <nathanielnyberg2021@u.northwestern.edu>
2020-09-24 17:29:33 -0500

    Added Readme and cleaned up code
    Added Readme and cleaned up code

--- Astar.py
+++ Astar.py
@@ -1,16 +1,50 @@
 import pygame
 import random
 import tkinter as tk
-from tkinter import simpledialog
+from tkinter import *
 from tkinter import messagebox
+
+
+#########################################
+## Constants
+#########################################
+
+# Define colors by RGB color code
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0,0,255)
+YELLOW = (255,255,0)
+PURPLE = (128,0,128)
+
+# This sets the wdith of the screeen and number of rows and columns in the grid
+WIDTH = 800
+WINDOW_SIZE = [WIDTH, WIDTH]
+ROWS = 50       # This is both the number of rows and columns
+
+# This sets the margin between each cell
+MARGIN = 2
+
+# Side length of grid square
+LENGTH = (WIDTH - ROWS*MARGIN) // ROWS
+
+# Percent of grid spaces to make obstacles when randomly generating terrain (choose number 0-1)
+PERCENT = 0.3
+
+# Tkinter window
+ROOT = Tk()
+
 
 #########################################
 ## Helper Functions
 #########################################
 
-def drawGrid():
+def drawGrid(grid):
+    '''
+    This function takes a 2D list of nodes and draws the grid in pygame
 
-    'This function draws the node grid'
+    '''
 
     # Set the screen background
     screen.fill(BLACK)
@@ -18,6 +52,7 @@ def drawGrid():
     # Draw the grid
     for row in range(ROWS):
         for column in range(ROWS):
+            # 
             color = grid[row][column].color
             pygame.draw.rect(screen,
                              color,
@@ -29,12 +64,13 @@ def drawGrid():
         # Limit to 60 frames per second
     clock.tick(60)
  
-    # Go ahead and update the screen with what we've drawn.
+    # Update the screen with what we've drawn.
     pygame.display.flip()
 
 def getSuccessors(node):
-
-    'This function takes a node and returns its possible successors'
+    '''
+    This function takes a node and returns its possible successors (surrounding 8 nodes)
+    '''
 
     rows = [node.x,node.x-1,node.x+1]
     cols = [node.y,node.y-1,node.y+1]
@@ -50,9 +86,46 @@ def getSuccessors(node):
 
     return successors 
 
-def nodeDist(node1,node2):
+def updateColors(open,closed,end_path):
+    '''
+    This function takes the open list, closed list, and final node of the proposed optimal path and colors the 
+    nodes accordingly.
 
-    'This function calculates the euclidian distance between two nodes'
+    Variables: 
+    open - A* algorithm open list
+    closed - A* algorithm closed list
+    end_path - Newest node to be evaluated/added to the path
+
+    Colors:
+    Green - Belongs to the open list
+    Red - Belongs to the closed list
+    Purple - Part of the proposed optimal path
+
+    '''
+
+    # Color nodes in open list 
+    for node in open_list:
+        if node == start or node == end:
+            continue
+        node.color = GREEN
+        node.open = True
+
+    # Color nodes in closed list
+    for node in closed_list:
+        if node == start or node == end:
+            continue
+        node.color = RED
+        node.closed = True
+
+    # Color/display current proposed optimal path
+    while end_path.path != False:
+        end_path.color = PURPLE
+        end_path = end_path.path
+
+def nodeDist(node1,node2):
+    '''
+    This function calculates the euclidian distance between two nodes
+    '''
 
     dist = ((node1.x-node2.x)**2 + (node1.y-node2.y)**2)**.5
     return dist
@@ -62,17 +135,32 @@ def nodeDist(node1,node2):
 #########################################
 
 class Node:
+    '''
+    This is the node class, which contains useful information about each node in the map/grid
+    '''
 
     def __init__(self,x,y):
+
+        # Positional information
         self.x = x
         self.y = y
+
+        # Cost information
         self.g = 0
         self.h = 0
         self.f = 0
+
+        # Is this the start/finish?
         self.start = False
         self.end = False
+
+        # Color of Node
         self.color = WHITE
+
+        # Is an obstacle?
         self.obstacle = False
+
+        # In the closed/open list or path
         self.open  = False
         self.closed = False
         self.path = False
@@ -81,60 +169,56 @@ class Node:
 ## Visualization Setup
 #########################################
 
-# Uer input to specify random or manual setup
-ROOT = tk.Tk()
-ROOT.withdraw()
+# User input to specify random or manual setup
 
-setup = simpledialog.askstring(title="Grid Setup",
-                                  prompt="Would you like to build the grid randomly or manually? [R/M]")
+# Create interface
+Label(ROOT,text="Would you like to build the grid randomly or manually:").grid(row=0, sticky=W)
+randomly = IntVar()
+Checkbutton(ROOT, text="Randomly", variable=randomly).grid(row=1, sticky=W)
+manually = IntVar()
+Checkbutton(ROOT, text="Manually", variable=manually).grid(row=2, sticky=W)
 
+Button(ROOT, text='Okay', command=ROOT.quit).grid(row=3, sticky=W, pady=4)
+mainloop()
 
-# Define colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
-BLUE = (0,0,255)
-YELLOW = (255,255,0)
-PURPLE = (128,0,128)
+# Assign setup variable based on user input
+setup = ''
+if manually.get() < randomly.get():
+    setup = 'R'
+else:
+    setup = 'M'
 
-# This sets the number of rows and columns in the grid
-WIDTH = 800
-ROWS = 50
-
-# This sets the margin between each cell
-MARGIN = 2
-
-# Side length of grid square
-LENGTH = (WIDTH - ROWS*MARGIN) // ROWS
- 
-# Create a 2 dimensional array for the grid.
+# Create a 2 dimensional array for the grid containing all of the nodes
 grid = []
 for row in range(ROWS):
     grid.append([])
     for column in range(ROWS):
+        # Create a node for that position in the grid
         newNode = Node(row,column)
-        if 0.3 > random.random() and setup == 'R':
+        # Make random obstacles 
+        if PERCENT > random.random() and setup == 'R':
             newNode.obstacle = True
             newNode.color = BLACK
-        grid[row].append(newNode)  # Append a node
+        grid[row].append(newNode)  # Append the node
  
 
 # Provide User Instructions
-
+ROOT.withdraw()
+instructions = ''
 if setup == 'M':
-    messagebox.showinfo("Instructions", "Left click to add a start and then an end. Once start and end points "
-                                    "have been specfied, left click to add obstacles. Right click to remove"
-                                    " start, end, or obstacles. Press spacebar to run algorithm")
+    instructions = ("Left click to add a start and then an end. Once start and end points "
+                    "have been specfied, left click to add obstacles. Right click to remove"
+                    " start, end, or obstacles. Press spacebar to run algorithm")
 elif setup == 'R':
-    messagebox.showinfo("Instructions", "Left click to add a start and then an end. Right click to remove"
-                                    " start or end points. Press spacebar to run the algorithm")
+    instructions = ("Left click to add a start and then an end. Right click to remove"
+                    " start or end points. Press spacebar to run the algorithm")
+
+messagebox.showinfo("Instructions",instructions)
 
 # Initialize pygame
 pygame.init()
  
 # Set the size of the screen
-WINDOW_SIZE = [WIDTH, WIDTH]
 screen = pygame.display.set_mode(WINDOW_SIZE)
  
 # Set title of screen
@@ -144,25 +228,21 @@ pygame.display.set_caption("A* Pathfinding Algorithm")
 done = False        # Algorithm in process if False
 start = False       # Start position has been chosen
 end = False         # End position has been chosen
-Started = False     # Has algorithm
+Started = False     # Has algorithm started?
  
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
 
 
 #########################################
-## A* Algorithm Setup
-#########################################
-
-# Initialize the open list
-open_list = []
-
-# Initialize the closed list
-closed_list = []
- 
-#########################################
 ## Main Program Loop
 #########################################
+
+# Initialize the A* open list
+open_list = []
+
+# Initialize the A* closed list
+closed_list = []
 
 while not done:
 
@@ -177,7 +257,7 @@ while not done:
             if event.key == pygame.K_SPACE and start != False and end != False:
                 Started = True
         if Started:
-            continue
+            break
 
         # User grid component setup/creation
         if pygame.mouse.get_pressed()[0]:
@@ -236,87 +316,60 @@ while not done:
 
     # -------------- A* Algorithm -------------- 
 
-    if Started == True:
+    if Started:
 
         # Quit if open list is empty
         if not open_list:
-            done = True
             print('No Path Found!')
+            continue
         
-        # Find the node with the lowest f-value in the open list, q
-        q = open_list[0]
-        for i in open_list:
-            if i.f < q.f:
-                q = i
+        # Find the node with the lowest f-value in the open list, current_node
+        current_node = open_list[0]
+        for node in open_list:
+            if node.f < current_node.f:
+                current_node = node
     
-        # If q is the destination, then we are done.
-        if q == end:
-            done = True
+        # If current_node is the destination, then we are done.
+        if current_node == end:
+            continue
 
-        # Remove q from the open_list 
-        open_list.remove(q)
+        # Remove current_node from the open_list 
+        open_list.remove(current_node)
 
-        # Add q to closed list
-        closed_list.append(q)
+        # Add current_node to closed list
+        closed_list.append(current_node)
 
-        # Get successors of node q, successors are the next nodes to be checked
-        successors = getSuccessors(q)
+        # Get successors of current node, successors are the next nodes to be checked
+        successors = getSuccessors(current_node)
 
         # Evaluate successor nodes
-        for i in successors:
+        for next_node in successors:
             # If already evaluated completely (in closed list) skip the node
-            if i in closed_list:
+            if next_node in closed_list:
                 continue
             
             # Calculate g score for this node on this current path
-            tempG = q.g + nodeDist(q,i)
+            tempG = current_node.g + nodeDist(current_node,next_node)
 
             # If node is new, add it to the open list. If the node is note new but is already part
             # of a better path, skip the node.
-            if i not in open_list:
-                open_list.append(i)
-            elif tempG >= i.g:
+            if next_node not in open_list:
+                open_list.append(next_node)
+            elif tempG >= next_node.g:
                 continue
 
             # The node is either new or can create a shorter path, so we add it to the path and 
             # update its cost
-            i.path = q
-            i.g = tempG
-            i.h = nodeDist(i,end)
-            i.f = i.g + i.h
+            next_node.path = current_node
+            next_node.g = tempG
+            next_node.h = nodeDist(next_node,end)
+            next_node.f = next_node.g + next_node.h
 
-           
-        # Color nodes in open list 
-        for i in open_list:
-            if i == start or i == end:
-                continue
-            i.color = GREEN
-            i.open = True
-
-        # Color nodes in closed list
-        for i in closed_list:
-            if i == start or i == end:
-                continue
-            i.color = RED
-            i.closed = True
-
-        # Color/display current proposed optimal path
-        while q.path != False:
-            q.color = PURPLE
-            q = q.path
-
+            updateColors(open_list,closed_list,current_node)
 
     # Draw the grid
-    drawGrid()
+    drawGrid(grid)
  
-
-# This is my weak way to keep the final path displayed until the user closes the window
-finished = False   
-while not finished:
-
-    for event in pygame.event.get():  # User did something
-        if event.type == pygame.QUIT:  # If user clicked close
-            finished = True  # Flag that we are done so we exit this loop
 
 # End the pygame instance
 pygame.quit()
--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# A-Search
+# A* Search Algorithm
+
+During my junior year at Northwestern, I became very interested in motion planning and controls. I’d heard of path finding algorithms and seen some examples of them in use and wanted to learn more. 
+
+I decided to write a program that would implement the A* search algorithm and actively give me a visualization of the method. When running the program, you will be able to choose whether you would like to randomly generate the terrain or if you would like to manually place obstacles.
+
+## Getting Started
+
+Successfully running the program should be extremely simple:
+
+1. Clone the repository. You can find information on how to clone a repository from Github [here](https://docs.github.com/en/github/creating-cloning-and-archiving-repositories/cloning-a-repository)
+2. Set the PERCENT constant to whatever percent of the terrain you want to be made up of obstacles (when randomly generated)
+3. Run Astar.py
+
+### Prerequisites
+
+I'm writing in Python using Visual Studio Code which is included with Anaconda. I would recommend that you use both of these and you can find information on how to install and set up Anaconda on your laptop [here](https://docs.anaconda.com/anaconda/install/)
+
+I'm also using the following Python libraries:
+1. pygame
+2. tkinter
+3. random
+
+You will have to install these if you would like to run this code. You can easily install these using pip which also is included with Anaconda. To install these Python packages using pip, follow these steps:
+
+1. Open the Anaconda Prompt
+2. Enter the command 'pip install <library_name>'. User the library names from above to install the necessary packages.
+
+
+
+
+
